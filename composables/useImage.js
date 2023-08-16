c7703677e3d5bb7c1bc9d9f1c705eba6743c0061
jcruzdev6 <125368583+jcruzdev6@users.noreply.github.com>
2023-08-16 12:12:56 +0800

    updated useImage to handle null values

--- composables/useImage.js
+++ composables/useImage.js
@@ -3,23 +3,27 @@ export default function useImage(type, id, type_id) {
     let image;
     switch (type) {
         case 'facility_type':
-            assets = import.meta.glob('@/assets/images/facilities/*/home.jpg', {eager: true});   
-            if (assets['/assets/images/facilities/' + id + '/home.jpg'])   
+            assets = import.meta.glob('@/assets/images/facilities/*/home.jpg', {eager: true});  
+            //handle null value 
+            if (assets['/assets/images/facilities/' + id + '/home.jpg'])
                 image = assets['/assets/images/facilities/' + id + '/home.jpg']["default"];
             break;
         case 'facility':
             assets = import.meta.glob('@/assets/images/facilities/*/*/*.jpg', {eager: true});      
             console.log('/assets/images/facilities/' + type_id + '/' + id + '/' + id + '-1.jpg');
+            //handle null value 
             if (assets['/assets/images/facilities/' + type_id + '/' + id + '/' + id + '-1.jpg'])
                 image = assets['/assets/images/facilities/' + type_id + '/' + id + '/' + id + '-1.jpg']["default"];
             break;
         case 'page':
             assets = import.meta.glob('@/assets/images/pages/*/*.jpg', {eager: true});      
+            //handle null value 
             if (assets['/assets/images/pages/' + id + '/header.jpg'])   
                 image = assets['/assets/images/pages/' + id + '/header.jpg']["default"];
             break;
         case 'page_cards':
             assets = import.meta.glob('@/assets/images/pages/*/*.jpg', {eager: true});      
+            //handle null value 
             if (assets['/assets/images/pages/' + id + '/header.jpg'])   
                 image = assets['/assets/images/pages/' + id + '/header.jpg']["default"];
             break;
